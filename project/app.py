17cbb48af1ecb3a83f9db4a36181bfe1665c08d2
Hugh Hsieh <tsungsyu@gmail.com>
2019-04-24 14:31:38 +1000

    Init project

---
+++ project/app.py
@@ -0,0 +1,42 @@
+import couchdb
+from flask import Flask, g, render_template
+
+app = Flask(__name__)
+
+
+def get_couch_db():
+    if not hasattr(g, 'couch_db'):
+        g.couch_db = couchdb.Server("http://localhost:5984/")
+    return g.couch_db
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def add():
+    dbname = "wta2019"
+    couchdb = get_couch_db()
+    db = couchdb[dbname] if dbname in couchdb else couchdb.create(dbname)
+    saved_ids = list()
+    doc = {'name': 'Garbiñe Muguruza'}
+    db.save(doc)
+    saved_ids.append(doc)
+    doc = {'name': ' Hsieh Su-wei'}
+    db.save(doc)
+    saved_ids.append(doc)
+
+    return str(saved_ids)
+
+
+@app.route('/wta/2019/<uuid>')
+def query(uuid):
+    dbname = "wta2019"
+    db = get_couch_db()[dbname]
+    return db[uuid]['name']
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
