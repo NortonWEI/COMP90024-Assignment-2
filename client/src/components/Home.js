d49ec9117950118458e5fd9227cb4b1033ade546
jayai2014 <dafu.ai819@gmail.com>
2019-05-06 23:30:36 +1000

    added one more graph and connect to apis

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -13,7 +13,23 @@ import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
 import ShowChartIcon from "@material-ui/icons/ShowChart";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import ListItemText from "@material-ui/core/ListItemText";
-import {CartesianGrid, Legend, Scatter, ScatterChart, Tooltip, XAxis, YAxis, ZAxis} from "recharts";
+import {
+  Bar,
+  BarChart, Brush,
+  CartesianGrid,
+  Label,
+  Legend,
+  ReferenceLine,
+  Scatter,
+  ScatterChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+  ZAxis
+} from "recharts";
+import Button from "@material-ui/core/Button";
+import {Typography} from "@material-ui/core";
+import {SizeMe} from "react-sizeme";
 
 const lowerCaseKeysObject = obj => Object.fromEntries(
   Object.entries(obj).map(([k, v]) => [k.toLowerCase(), v])
@@ -25,48 +41,105 @@ class Home extends React.Component {
       e: "",
       geoJSONData: null,
       loading: true,
-      //geoJSONUrl: 'https://data.gov.au/geoserver/vic-suburb-locality-boundaries-psma-administrative-boundaries/wfs?request=GetFeature&typeName=ckan_af33dd8c_0534_4e18_9245_fc64440f742e&outputFormat=json',
+      loadingContent: '',
       geoJSONUrl: 'http://localhost/geoserver.geojson',
       bounds: null,
       sentimentData: {},
-      currentFeature: null
+      currentFeature: null,
+      allDataBySuburb: {}
   };
 
   async componentDidMount() {
     try {
       this.setState({
-        loading: true
+        loading: true,
+        loadingContent: '(1) Suburb Data'
       });
 
-      const data = await Axios.get(
+      const suburbData = await Axios.get(
         this.state.geoJSONUrl,
       );
+
+      let allData = {};
+
       this.setState({
-        geoJSONData: data.data,
+        loadingContent: '(2) Twitter Sentiment Data'
+      });
+
+      const sentimentData = await Axios.get(
+        'http://localhost:5000/tweets_sentiment/VIC',
+      );
+
+      Object.keys(sentimentData.data).forEach(s => {
+        const lc = s.toLowerCase();
+        if (!(lc in allData)) {
+          allData[lc] = {};
+        }
+
+        allData[lc]['sent'] = sentimentData.data[s];
+      });
+
+      this.setState({
+        loadingContent: '(3) Sickness Allowance Data'
+      });
+
+      const sicknessAllowanceData = await Axios.get(
+        'http://localhost:5000/sickness_allowance/VIC',
+      );
+
+      Object.keys(sicknessAllowanceData.data).forEach(s => {
+        const lc = s.toLowerCase();
+        if (!(lc in allData)) {
+          allData[lc] = {};
+        }
+
+        allData[lc]['sick'] = sicknessAllowanceData.data[s];
+      });
+
+      this.setState({
+        loadingContent: '(4) Mental Health Data'
+      });
+
+      const mentalHealthData = await Axios.get(
+        'http://localhost:5000/mental_health/VIC',
+      );
+
+      Object.keys(mentalHealthData.data).forEach(s => {
+        const lc = s.toLowerCase();
+        if (!(lc in allData)) {
+          allData[lc] = {};
+        }
+
+        allData[lc]['mental'] = mentalHealthData.data[s];
+      });
+
+      this.setState({
+        geoJSONData: suburbData.data,
+        allDataBySuburb: allData,
         loading: false,
-      })
+      });
+
+      console.log(allData);
     } catch (e) {
 
     }
-
-    const sentimentData = {"Cambarville":-0.665,"Colac West":-0.214,"Connewarre":-0.859,"Coolaroo":-0.457,"Costerfield":-0.823,"Fitzroy North":0.274,"Fyansford":-0.469,"Garden City":0.834,"Glen Alvie":-0.747,"Glenferrie South":-0.256,"Gordon":-0.144,"Hinnomunjie":0.214,"Indigo Valley":0.134,"Joel South":0.16,"Kialla East":0.584,"Labertouche":0.293,"Lancefield":0.161,"Mount Eliza":-0.979,"Murchison North":0.515,"Narre Warren":0.816,"Neerim Junction":-0.59,"Oakleigh South":0.916,"Ormond":-0.984,"Surrey Hills":0.412,"Toongabbie":-0.042,"Tresco":-0.539,"Tynong":-0.094,"Woorarra East":-0.419,"Wyndham Vale":-0.064,"Yarrambat":0.537};
-
-    this.setState({
-      sentimentData: lowerCaseKeysObject(sentimentData)
-    })
   }
 
-  hasSuburb = suburb => {
-    return suburb in this.state.sentimentData;
+  hasSuburbSentiment = suburb => {
+    return suburb in this.state.allDataBySuburb && 'sent' in this.state.allDataBySuburb[suburb];
+  };
+
+  getSuburbSentiment = suburb => {
+    return this.state.allDataBySuburb[suburb]['sent'];
   };
 
   getSentimentColorBySuburb = suburb => {
     let val;
-    if (!this.hasSuburb(suburb)) {
+    if (!this.hasSuburbSentiment(suburb)) {
       // val = -1 + (Math.random() * 2);
       val = 0;
     } else {
-      val = this.state.sentimentData[suburb];
+      val = this.getSuburbSentiment(suburb);
     }
 
     return this.getSentimentColor(val)
@@ -76,18 +149,15 @@ class Home extends React.Component {
     const red = Math.floor(Math.max(255*(-val), 0));
     const green = Math.floor(Math.max(255*(val), 0));
 
-    const color = `rgb(${red},${green},0)`;
-
-    //console.log(suburb + ' ' + color);
-    return color;
+    return `rgb(${red},${green},0)`;
   };
 
   getFillOpacity = suburb => {
-    if (!this.hasSuburb(suburb)) {
+    if (!this.hasSuburbSentiment(suburb)) {
       return 0.5;
     }
 
-    const val = this.state.sentimentData[suburb];
+    const val = this.getSuburbSentiment(suburb);
     return Math.max(Math.abs(val) * 0.9, 0.3);
   };
 
@@ -138,6 +208,43 @@ class Home extends React.Component {
     });
   };
 
+  renderSuburbInfoPanel = (suburb) => {
+    const lcSuburb = suburb.toLowerCase();
+    const temp = this.state.allDataBySuburb[lcSuburb];
+    const suburbData = temp ? temp : {'sent':'Unavailable', 'sick': 'Unavailable', 'mental': 'Unavailable'};
+
+    return (
+      <Paper style={{background: "rgba(255,255,255,0.8)"}} >
+       <List component="nav">
+          <ListItem>
+            <ListItemIcon>
+              <LocationOnIcon />
+            </ListItemIcon>
+            <ListItemText primary={suburb} />
+          </ListItem>
+         <ListItem>
+          <ListItemIcon>
+            <InsertEmoticonIcon />
+          </ListItemIcon>
+          <ListItemText primary={suburbData['sent']} secondary={'Twitter Sentiment'}/>
+         </ListItem>
+         <ListItem>
+          <ListItemIcon>
+            <AttachMoneyIcon />
+          </ListItemIcon>
+          <ListItemText primary={suburbData['sick']} secondary={'Sickness Allowance'}/>
+         </ListItem>
+         <ListItem>
+          <ListItemIcon>
+          <ShowChartIcon />
+          </ListItemIcon>
+          <ListItemText primary={suburbData['mental']} secondary={'Mental Health Issue Cases'}/>
+         </ListItem>
+      </List>
+     </Paper>
+    );
+  };
+
   renderMap = () => {
     const position = [-37.8136, 144.9631];
     const currSuburb = this.state.currentFeature ? this.state.currentFeature.properties.vic_loca_2 : null;
@@ -154,36 +261,7 @@ class Home extends React.Component {
           ref="geojson"
         />
         <Control position="topright">
-          {this.state.currentFeature && (
-            <Paper style={{background: "rgba(255,255,255,0.8)"}} >
-             <List component="nav">
-                <ListItem>
-                  <ListItemIcon>
-                    <LocationOnIcon />
-                  </ListItemIcon>
-                  <ListItemText primary={currSuburb} />
-                </ListItem>
-               <ListItem>
-                <ListItemIcon>
-                  <InsertEmoticonIcon />
-                </ListItemIcon>
-                <ListItemText primary={this.state.sentimentData[currSuburb.toLowerCase()]} secondary={'Twitter Sentiment'}/>
-               </ListItem>
-               <ListItem>
-                <ListItemIcon>
-                  <AttachMoneyIcon />
-                </ListItemIcon>
-                <ListItemText primary={'xxx'} secondary={'Sickness Allowance'}/>
-               </ListItem>
-               <ListItem>
-                <ListItemIcon>
-                <ShowChartIcon />
-                </ListItemIcon>
-                <ListItemText primary={'xxx'} secondary={'Mental Health Issue Cases'}/>
-               </ListItem>
-            </List>
-           </Paper>
-          )}
+          {this.state.currentFeature && this.renderSuburbInfoPanel(currSuburb)}
         </Control>
         <Control position="bottomright">
            <Paper style={{background: "rgba(255,255,255,0.8)", padding: "6px"}} >
@@ -204,7 +282,6 @@ class Home extends React.Component {
                </Grid>
                <Grid item xs style={{textAlign: "right"}}>1</Grid>
              </Grid>
-
            </Paper>
         </Control>
       </Map>
@@ -220,7 +297,7 @@ class Home extends React.Component {
               <CircularProgress />
             </Grid>
             <Grid item>
-              Loading map data...
+              Loading {this.state.loadingContent} ...
             </Grid>
           </Fragment>
         )}
@@ -229,39 +306,122 @@ class Home extends React.Component {
     )
   };
 
-  renderCharts = () => {
-    let data = [
-      { suburb: 'MELBOURNE', x: -0.8, y: 800, z: 200 }, { suburb: 'SOUTH MELBOURNE', x: 0.5, y: 50, z: 555 },
-      { suburb: 'SOUTHBANK', x: -0.5, y: 500, z: 170 }, { suburb: 'EAST MELBOURNE', x: 0, y: 100, z: 300 },
-      { suburb: 'CARLTON', x: 1, y: 10, z: 1000 }, { suburb: 'DOCKLANDS', x: 0.3, y: 30, z: 333 },
-    ];
+  renderTopStats = () => {
+    let sortedSuburbData = Object.keys(this.state.allDataBySuburb).map(s => {
+      return {
+        ...this.state.allDataBySuburb[s],
+        suburb: s,
+      }
+    });
 
-    data.sort((d1, d2) => d2.x - d1.x);
+    sortedSuburbData.sort((d1, d2) => d2.sent - d1.sent);
+
+    const positive = sortedSuburbData.filter(d => d.sent >= 0).map(d => {return {...d, sPositive: d.sent};});
+    const negative = sortedSuburbData.filter(d => d.sent < 0).map(d => {return {...d, sNegative: d.sent};});
+    const recombined = [...positive, ...negative];
 
     return (
       <Grid item xs={12} container justify="center">
-        <Grid item>
-          <ScatterChart
-            width={800}
-            height={400}
-            margin={{
-              top: 20, right: 20, bottom: 20, left: 20,
-            }}
-          >
-            <CartesianGrid />
-            <XAxis type="number" dataKey="x" name="sentiment" unit="" />
-            <YAxis yAxisId="left" type="number" dataKey="y" name="cases" unit="" />
-            <YAxis yAxisId="right" orientation="right" type="number" dataKey="z" name="allowance" unit="$" />
-            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-            {/*<Legend />*/}
-            {data.map((d, index) => (
-                <Scatter yAxisId="left" key={index} name={`${d.suburb}`} data={[d]} fill={this.getSentimentColor(d.x)} shape="circle" />
-            ))}
-            {data.map((d, index) => (
-                <Scatter yAxisId="right" key={index} name={`${d.suburb}`} data={[d]} fill={this.getSentimentColor(d.x)} shape="circle" />
-            ))}
-          </ScatterChart>
+        <Grid item xs={12}>
+          <Typography variant="h6">Sentiment Score by Suburb</Typography>
         </Grid>
+         <SizeMe>{({ size }) => <Grid item xs={12}>
+            <BarChart
+              width={size.width - 40}
+              height={300}
+              data={recombined}
+              margin={{
+                top: 20, right: 20, bottom: 20, left: 20,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="suburb" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <ReferenceLine y={0} stroke="#000" />
+              <Bar dataKey="sPositive" fill="#00cc44" name="+ Avg. Twitter Sentiment"/>
+              <Bar dataKey="sNegative" fill="#ff4d4d"  name="- Avg. Twitter Sentiment"/>
+
+            </BarChart>
+         </Grid>
+        }
+        </SizeMe>
+      </Grid>
+
+    );
+  };
+
+  renderCharts = () => {
+    // let data = [
+    //   { suburb: 'MELBOURNE', x: -0.8, y: 800, z: 200 }, { suburb: 'SOUTH MELBOURNE', x: 0.5, y: 50, z: 555 },
+    //   { suburb: 'SOUTHBANK', x: -0.5, y: 500, z: 170 }, { suburb: 'EAST MELBOURNE', x: 0, y: 100, z: 300 },
+    //   { suburb: 'CARLTON', x: 1, y: 10, z: 1000 }, { suburb: 'DOCKLANDS', x: 0.3, y: 30, z: 333 },
+    // ];
+    //
+    // data.sort((d1, d2) => d2.x - d1.x);
+
+    let sortedSuburbData = Object.keys(this.state.allDataBySuburb).map(s => {
+      return {
+        ...this.state.allDataBySuburb[s],
+        suburb: s,
+      }
+    });
+
+    sortedSuburbData.sort((d1, d2) => d2.sent - d1.sent);
+
+    return (
+      <Grid item xs={12} container justify="center">
+        <Grid item xs={12}>
+          <Typography variant="h6">Sentiment Score vs. Mental Health Issue Cases</Typography>
+        </Grid>
+        <SizeMe>{({ size }) => <Grid item xs={12} container justify="center">
+          <Grid item>
+            <ScatterChart
+              width={size.width - 40}
+              height={500}
+              margin={{
+                top: 20, right: 20, bottom: 20, left: 20,
+              }}
+            >
+              <CartesianGrid />
+              <XAxis type="number" dataKey="mental" name="cases" unit="">
+                <Label value="Avg. Mental Health Issue Cases" offset={0} position="insideBottom" />
+              </XAxis>
+              <YAxis type="number" label={{ value: 'Avg. Twitter Sentiment', angle: -90, position: 'insideLeft' }} dataKey="sent" name="sentiment" unit="" />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Legend verticalAlign="bottom" height={200}/>
+              {sortedSuburbData.map((d, index) => (
+                  <Scatter key={index} name={`${d.suburb}`} data={[d]} fill={this.getSentimentColor(d.sent)} shape="circle" />
+              ))}
+            </ScatterChart>
+          </Grid>
+        </Grid>}</SizeMe>
+        <Grid item xs={12}>
+          <Typography variant="h6">Sentiment Score vs. Sickness Allowance</Typography>
+        </Grid>
+        <SizeMe>{({ size }) => <Grid item xs={12} container justify="center">
+          <Grid item>
+            <ScatterChart
+              width={size.width - 40}
+              height={500}
+              margin={{
+                top: 20, right: 20, bottom: 20, left: 20,
+              }}
+            >
+              <CartesianGrid />
+              <XAxis type="number" dataKey="sick" name="allowance" unit="$">
+                <Label value="Avg. Sickness Allowance" offset={0} position="insideBottom" />
+              </XAxis>
+              <YAxis type="number" label={{ value: 'Avg. Twitter Sentiment', angle: -90, position: 'insideLeft' }} dataKey="sent" name="sentiment" unit="" />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Legend verticalAlign="bottom"/>
+              {sortedSuburbData.map((d, index) => (
+                  <Scatter key={index} name={`${d.suburb}`} data={[d]} fill={this.getSentimentColor(d.sent)} shape="circle" />
+              ))}
+            </ScatterChart>
+          </Grid>
+        </Grid>}</SizeMe>
       </Grid>
     )
   };
@@ -270,6 +430,7 @@ class Home extends React.Component {
     return (
         <Grid container spacing={24}>
           {this.renderMapSection()}
+          {this.renderTopStats()}
           {this.renderCharts()}
         </Grid>
     );
