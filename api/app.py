6f6ecbc7e951098028ae9797a6e6f21d03354666
Hugh Hsieh <tsungsyu@gmail.com>
2019-05-07 13:19:43 +1000

    2 API connect to view: sickness_allowance and mental_health
    my_test_view method is only for temporal purpose

--- api/app.py
+++ api/app.py
@@ -1,11 +1,13 @@
 import couchdb
 import json
-from flask import Flask, g, jsonify, url_for
+from flask import Flask, g, jsonify
 import random
 from flask_cors import CORS
+import re
 
 GEO_INFO_FILE = "config/suburbs.json"
 JSON_MIME_TYPE = 'application/json'
+LGA_PREFIX_FILE = "config/lga_prefix.json"
 TEST_QUOTA = 50
 
 app = Flask(__name__)
@@ -24,7 +26,7 @@ class Suburb:
 
 def get_couch_db():
     if not hasattr(g, 'couch_db'):
-        g.couch_db = couchdb.Server("http://couchdb:5984")
+        g.couch_db = couchdb.Server("http://{}:{}@couchdb:5984".format("user", "pass"))
     return g.couch_db
 
 
@@ -38,6 +40,19 @@ def get_geo_info():
                            suburb_json['lga_name'], suburb_json['state_code'], suburb_json['state'])
                 g.geo_info.append(s)
     return g.geo_info
+
+
+def get_lga_prefix():
+    if not hasattr(g, 'lga_prefix'):
+        with open(LGA_PREFIX_FILE, 'r') as f:
+            g.lga_prefix = json.load(f)
+    return g.lga_prefix
+
+
+def get_statistic_view(collection_name, design_doc_name, view_name, start_key, end_key):
+    return get_couch_db()[collection_name].view('{}/{}'.format(design_doc_name, view_name),
+                                                startkey=[start_key],
+                                                endkey=[end_key, "{}"])
 
 
 @app.route('/')
@@ -64,26 +79,61 @@ def tweets_sentiment(state):
 
 @app.route('/sickness_allowance/<state>')
 def sickness_allowance(state):
-    suburbs = [sub for sub in get_geo_info() if sub.state_code == state]
+    state = state.upper()
     view = dict()
-    access_index = 0
-    while len(view) < TEST_QUOTA:
-        #access_index = random.randint(0, len(suburbs) - 1)
-        access_index += 1
-        view[suburbs[access_index].suburb] = round(random.uniform(0, 200), 3)
+    start_index = "{}0000".format(get_lga_prefix()[state])
+    end_index = "{}0000".format(int(get_lga_prefix()[state]) + 1)
+    for item in get_statistic_view("medipayment", "sickness_allowance", "sickness_allowance", start_index, end_index):
+        view[re.sub(r'\(\S+\)', '', item.key[1]).strip()] = item.value
     return jsonify(view)
 
 
 @app.route('/mental_health/<state>')
 def mental_health(state):
-    suburbs = [sub for sub in get_geo_info() if sub.state_code == state]
+    state = state.upper()
     view = dict()
-    access_index = 0
-    while len(view) < TEST_QUOTA:
-        #access_index = random.randint(0, len(suburbs) - 1)
-        access_index += 1
-        view[suburbs[access_index].suburb] = random.randint(1, 200)
+    start_index = "{}0000".format(get_lga_prefix()[state])
+    end_index = "{}0000".format(int(get_lga_prefix()[state]) + 1)
+    for item in get_statistic_view("mentalhealthadmission", "distribution", "mental_health_view", start_index, end_index):
+        view[re.sub(r'\(\S+\)', '', item.key[1]).strip()] = item.value
     return jsonify(view)
+
+# TODO only for generating data
+@app.route('/my_test_view/<state>')
+def my_test_view(state):
+    lga_prefixs = get_lga_prefix()
+    start_index = lga_prefixs[state]
+    end_index = str(int(start_index) + 1)
+    couchdb = get_couch_db()
+    dbname = "medipayment"
+    intersections = dict()
+
+    for item in couchdb[dbname].view('sickness_allowance/sickness_allowance', startkey=["{}0000".format(start_index)], endkey=["{}0000".format(end_index), "{}"]):
+        intersections[(re.sub(r'\(\S+\)', '', item.key[1]).strip(), item.key[0])] = 0
+
+    dbname = "mentalhealthadmission"
+    for item in couchdb[dbname].view('distribution/mental_health_view', startkey=["{}0000".format(start_index)], endkey=["{}0000".format(end_index), "{}"]):
+        area = (re.sub(r'\(\S+\)', '', item.key[1]).strip(), item.key[0])
+        if area in intersections.keys():
+            intersections[area] = 1
+
+    intersections = {k[0]: k[1] for k, v in intersections.items() if v > 0}
+    suburbs = [sub for sub in get_geo_info() if sub.state_code == state]
+    out_dict = dict()
+
+    for name, code in intersections.items():
+        if name not in out_dict.keys():
+            out_dict[name] = list()
+
+        for sub in suburbs:
+            if code == sub.lga_code:
+                s = dict()
+                s["postcode"] = sub.postcode
+                s["suburb"] = sub.suburb
+                out_dict[name].append(s)
+
+    print(out_dict)
+    return "{} of suburbs and {} of local government areas".format(len(suburbs),len(out_dict))
 
 
 @app.route('/', methods=['POST'])
