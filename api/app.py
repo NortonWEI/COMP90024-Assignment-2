e27ad3e49313c75210cec98b13db5138ac5a4157
Hugh Hsieh <tsungsyu@gmail.com>
2019-05-10 15:44:47 +1000

    Monitoring API
    online return the highest sentiment fluctuation 500 users

--- api/app.py
+++ api/app.py
@@ -1,16 +1,16 @@
 import couchdb
 import json
 from flask import Flask, g, jsonify
-import random
 from flask_cors import CORS
 import re
+from collections import Counter
 
 GEO_INFO_FILE = "config/suburbs.json"
 JSON_MIME_TYPE = 'application/json'
 LGA_PREFIX_FILE = "config/lga_prefix.json"
 MELBOURNE_AREA_SUBURBS = "config/melbourne-suburbs.json"
 LGA_COMPOSITION_FILE = "config/lga_composition.json"
-TEST_QUOTA = 50
+PAGE_ROWS = 500
 
 app = Flask(__name__)
 CORS(app)
@@ -91,6 +91,7 @@ def tweets_sentiment(state):
                                                      endkey=[end_index, "{}"],
                                                      group=True, group_level=2):
         suburb = item.key[1]
+        # limiting of showing suburbs in Melbourne area
         if suburb.lower() in melbourne_suburbs:
             view[item.key[1]] = round(item.value["mean"], 3)
     return jsonify(view)
@@ -131,82 +132,35 @@ def mental_health(state):
                 view[suburb["suburb"]] = item.value
     return jsonify(view)
 
-# TODO only for generating data
-@app.route('/my_test_view/<state>')
-def my_test_view(state):
-    lga_prefixs = get_lga_prefix()
-    start_index = lga_prefixs[state]
-    end_index = str(int(start_index) + 1)
-    couchdb = get_couch_db()
-    dbname = "medipayment"
-    intersections = dict()
-
-    for item in couchdb[dbname].view('sickness_allowance/sickness_allowance', startkey=["{}0000".format(start_index)], endkey=["{}0000".format(end_index), "{}"]):
-        intersections[(re.sub(r'\(\S+\)', '', item.key[1]).strip(), item.key[0])] = 0
-
-    dbname = "mentalhealthadmission"
-    for item in couchdb[dbname].view('distribution/mental_health_view', startkey=["{}0000".format(start_index)], endkey=["{}0000".format(end_index), "{}"]):
-        area = (re.sub(r'\(\S+\)', '', item.key[1]).strip(), item.key[0])
-        if area in intersections.keys():
-            intersections[area] = 1
-
-    intersections = {k[0]: k[1] for k, v in intersections.items() if v > 0}
-    suburbs = [sub for sub in get_geo_info() if sub.state_code == state]
-    out_dict = dict()
-
-    for name, code in intersections.items():
-        if name not in out_dict.keys():
-            out_dict[name] = list()
-
-        for sub in suburbs:
-            if code == sub.lga_code:
-                s = dict()
-                s["postcode"] = sub.postcode
-                s["suburb"] = sub.suburb
-                out_dict[name].append(s)
-
-    print(out_dict)
-    return "{} of suburbs and {} of local government areas".format(len(suburbs),len(out_dict))
-
 
 @app.route('/monitoring')
 def monitoring():
     view = list()
-    coordinates = [(-37.7984, 144.9449), (-37.7833, 144.95), (-37.7603, 144.9502), (-37.8, 144.9667),
-                   (-37.8004, 144.9681), (-37.7818, 144.9666), (-37.7833, 144.9667), (-37.7603, 144.9502),
-                   (-37.7646, 144.9438), (-37.7603, 144.9502), (-37.7603, 144.9502), (-37.7667, 144.9667),
-                   (-37.7667, 144.9667), (-37.7667, 144.9667), (-37.7603, 144.9502), (-37.7725, 144.9724)]
-    while len(view) < TEST_QUOTA:
-        case = dict()
-        coordinate = coordinates[random.randint(0, len(coordinates) - 1)]
-        case["latitude"] = coordinate[0]
-        case["longitude"] = coordinate[1]
-        case["sentiment"] = round(random.uniform(-1, 1), 3)
-        case["fluctuation"] = round(random.uniform(0, 2), 3)
-        view.append(case)
+    users = dict()
+    rankings = Counter()
+    state = 'VIC'
+    start_index = "{}0000".format(get_lga_prefix()[state])
+    end_index = "{}9999".format(get_lga_prefix()[state])
+    melbourne_suburbs = get_melbourne_suburbs()
+    for item in get_couch_db()['tweets_sample'].view('tweets/user',
+                                                     startkey=[start_index],
+                                                     endkey=[end_index, "{}"],
+                                                     group=True, group_level=2):
+        if item.value["suburb"].lower() in melbourne_suburbs:
+            case = dict()
+            case["latitude"] = item.value["geo_coordinates"][1]
+            case["longitude"] = item.value["geo_coordinates"][0]
+            mag_positive = abs(item.value['positive_mean'])
+            mag_negative = abs(item.value['negative_mean'])
+            case["sentiment"] = round(mag_positive - mag_negative, 3)
+            case["fluctuation"] = round(mag_positive + mag_negative, 3)
+            users[item.key[1]] = case
+            rankings[item.key[1]] = case["fluctuation"]
+
+    ls = rankings.most_common(PAGE_ROWS)
+    for l in ls:
+        view.append(users[l[0]])
     return jsonify(view)
-
-@app.route('/', methods=['POST'])
-def add():
-    dbname = "wta2019"
-    couchdb = get_couch_db()
-    db = couchdb[dbname] if dbname in couchdb else couchdb.create(dbname)
-    saved_ids = list()
-    doc = {'name': 'Garbiñe Muguruza'}
-    db.save(doc)
-    saved_ids.append(doc)
-    doc = {'name': ' Hsieh Su-wei'}
-    db.save(doc)
-    saved_ids.append(doc)
-
-    return str(saved_ids)
-
-
-@app.route('/wta/2019/<uuid>')
-def query(uuid):
-    dbname = "wta2019"
-    db = get_couch_db()[dbname]
-    return db[uuid]['name']
 
 
 if __name__ == '__main__':
